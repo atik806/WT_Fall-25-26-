2834e2422a4d2b0b5d7929b2ed9f579955a67a1d
atik806 <atikrj8@gmail.com>
2025-11-01 19:09:19 +0600

    Login

---
+++ Final/Project/login.php
@@ -0,0 +1,64 @@
+<!Doctype html>
+<html>
+    <head>
+        <title>Login Page</title>
+
+        <style>
+            body{
+                background-color: white;
+                font-family: Times new roman, sans-serif;
+            }
+            h2{
+                color: black;
+                font-size: 40px;
+            }
+            .a{
+                font-size: 20px;
+                color: black;
+            }
+            .b{
+                font-size: 20px;
+                color: black;
+            }
+            .button{
+                background-color: black;
+                color: white;
+                padding: 10px 20px;
+                border: none;
+                border-radius: 5px;
+                cursor: pointer;
+                font-size: 16px;
+            }
+
+        </style>
+    </head>
+    <body>
+        <center>
+
+        <h2>Login</h2>
+        welcome back please login <br/><br/>
+        <table><tr>
+            <td class="a">
+                Email:
+            </td>
+        </tr>
+
+        <tr><td>
+            <input type="text" name="email" placeholder="Enter your email"/><br/><br/>
+        </td></tr>
+        <tr><td class = "b">
+            Password:
+        </td></tr>
+        <tr><td>
+             <input type="password" name="password" placeholder="Enter your password"/><br/><br/>
+        </td></tr>
+       
+        </table>
+        
+        <center>
+        <input type="Submit" class="button" value="Login"><br/><br/>
+    </body>
+</html>
+
+       
+        
