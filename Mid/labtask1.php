65ec2b110e7e78c3ae29f6a24a1d33fbb823fddf
atik806 <atikrj8@gmail.com>
2025-11-02 14:42:58 +0600

    Task1

--- Mid/labtask1.php
+++ Mid/labtask1.php
@@ -1,10 +1,180 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Lab Task 1</title>
+<title>Patient Registration</title>
+<style>
+    
+    .s1{
+        color: blue;
+        font-family: times new roman;
+        font-size: 40px;
+        text-alighn: center;
+    }
+    .s2{
+        text-align: left;
+    }
+    body{
+        background-color;
+        padding: 20px;
+        height: auto;
+        background-color: #f2f2f2;
+        
+    }
+    div{
+        
+        border-radius: 10px;
+        width: 600px;
+        height: 600px;
+        margin: 30px auto;
+        background-color: #fff2beff;
+        padding: 20px;
+    }
+    .a{
+        
+        color: blue
+    }
+    .button{
+        background-color: blue;
+                color: white;
+                padding: 10px 20px;
+                border: none;
+                border-radius: 5px;
+                cursor: pointer;
+                font-size: 10px;
+    }
+    .s3{
+        width: 100%;
+        : black;
+    }
+
+</style>
+
 </head>
 <body>
     
+    <center>
+        <h1 class="s1">Clinic patient Registration</h1>
+<div>
+<table>
+    <tr>
+        <td class="s2">
+            Full Name:
+            
+        </td>
+    </tr>
+
+    <tr>
+        <td class="s3">
+            <input type="text">
+        </td>
+    </tr>
+    <tr>
+        <td class="s2">
+            Age:
+        </td>
+    </tr>
+
+    <tr>
+        <td class="s3">
+            <input type="text">
+        </td>
+    </tr>
+    <tr>
+        <td class="s2">
+            Phone Number:
+        </td>
+    </tr>
+
+    <tr>
+        <td class="s3">
+            <input type="number">
+        </td>
+    </tr>
+    <tr>
+        <td class="s2">
+            Email:
+        </td>
+    </tr>
+
+    <tr>
+        <td class="s3">
+            <input type="text">
+        </td>
+    </tr>
+    <tr>
+        <td class="s2">
+            Insurance Provider:
+        </td>
+    </tr>
+
+    <tr>
+        <td class="s3">
+            <select> 
+                <option>Select Provider</option>
+                <option>Provider 1</option>
+                <option>Provider 2</option>
+                <option>Provider 3</option>
+            </select>
+
+        </td>
+    </tr>
+    <tr>
+        <td class="s2">
+            Insurance policy number:
+        </td>
+    </tr>
+
+    <tr>
+        <td class="s3">
+            <input type="text">
+        </td>
+    </tr>
+    <tr><td class = "a"><h1><center>Additional Information</center></h1></td></tr>
+    
+    <tr>
+        <td class="s2">
+            Username:
+        </td>
+    </tr>
+
+    <tr>
+        <td class="s3">
+            <input type="text">
+        </td>
+    </tr>
+    <tr>
+        <td class="s2">
+            Password:
+        </td>
+    </tr>
+
+    <tr>
+        <td class="s3">
+            <input type="text">
+        </td>
+    </tr>
+    <tr>
+        <td class="s2">
+            Confirm Password:
+        </td>
+    </tr>
+
+    <tr>
+        <td class="s3">
+            <input type="text">
+        </td>
+    </tr>
+
+
+    <tr><td>
+        <input type="Submit" class="button" value="Register"><br/><br/>
+    </td></tr>
+
+
+
+</center>
+</div>
+</table>
 </body>
 </html>
 
