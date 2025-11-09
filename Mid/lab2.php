624e337cc261c5c2ad924a38f7347b445b3efa3b
atik806 <atikrj8@gmail.com>
2025-11-09 14:30:41 +0600

    lab2

---
+++ Mid/lab2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<title>Task 2</title>
+</head>
+<body>
+    
+    <h1>Task 2 Completed</h1>
+<script>
+    document.write("Hello World");
+    var name = "John Doe";
+    var x;
+    console.log("Name: " + name);
+    console.log( typeof name);
+    console.log("Value of x: " + x);
+    console.log( typeof x);
+</script>
+</body>
