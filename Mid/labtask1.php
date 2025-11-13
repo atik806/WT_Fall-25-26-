b4eaa91a153bf9a87aaef39b50f494813472e735
atik806 <atikrj8@gmail.com>
2025-11-13 18:14:59 +0600

    updated lab task1

--- Mid/labtask1.php
+++ Mid/labtask1.php
@@ -3,178 +3,152 @@
 <head>
 <title>Patient Registration</title>
 <style>
-    
-    .s1{
-        color: blue;
-        font-family: times new roman;
-        font-size: 40px;
-        text-alighn: center;
-    }
-    .s2{
-        text-align: left;
-    }
-    body{
-        background-color;
-        padding: 20px;
-        height: auto;
+    body {
         background-color: #f2f2f2;
-        
+        font-family: "Times New Roman", serif;
+        padding: 20px;
     }
-    div{
-        
+
+    .s1 {
+        color: blue;
+        font-size: 40px;
+        text-align: center;
+        margin-bottom: 20px;
+    }
+
+    div {
         border-radius: 10px;
         width: 600px;
-        height: 600px;
         margin: 30px auto;
-        background-color: #fff2beff;
-        padding: 20px;
+        background-color: #fff2be;
+        padding: 30px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
     }
-    .a{
-        
-        color: blue
-    }
-    .button{
-        background-color: blue;
-                color: white;
-                padding: 10px 20px;
-                border: none;
-                border-radius: 5px;
-                cursor: pointer;
-                font-size: 10px;
-    }
-    .s3{
+
+    table {
         width: 100%;
-        : black;
     }
 
-</style>
+    .s2 {
+        text-align: left;
+        font-weight: bold;
+        padding-top: 10px;
+    }
 
+    .s3 input, .s3 select {
+        width: 100%;
+        padding: 8px;
+        border-radius: 5px;
+        border: 1px solid #ccc;
+        font-size: 14px;
+    }
+
+    .a {
+        color: blue;
+        text-align: center;
+        margin-top: 20px;
+    }
+
+    .button {
+        background-color: blue;
+        color: white;
+        padding: 10px 20px;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+        font-size: 16px;
+        margin-top: 20px;
+        width: 100%;
+    }
+
+    .button:hover {
+        background-color: darkblue;
+    }
+</style>
 </head>
 <body>
-    
-    <center>
-        <h1 class="s1">Clinic patient Registration</h1>
-<div>
-<table>
-    <tr>
-        <td class="s2">
-            Full Name:
-            
-        </td>
-    </tr>
+    <h1 class="s1">Clinic Patient Registration</h1>
 
-    <tr>
-        <td class="s3">
-            <input type="text">
-        </td>
-    </tr>
-    <tr>
-        <td class="s2">
-            Age:
-        </td>
-    </tr>
+    <div>
+        <form>
+            <table>
+                <tr>
+                    <td class="s2">Full Name:</td>
+                </tr>
+                <tr>
+                    <td class="s3"><input type="text" required></td>
+                </tr>
 
-    <tr>
-        <td class="s3">
-            <input type="text">
-        </td>
-    </tr>
-    <tr>
-        <td class="s2">
-            Phone Number:
-        </td>
-    </tr>
+                <tr>
+                    <td class="s2">Age:</td>
+                </tr>
+                <tr>
+                    <td class="s3"><input type="number" required></td>
+                </tr>
 
-    <tr>
-        <td class="s3">
-            <input type="number">
-        </td>
-    </tr>
-    <tr>
-        <td class="s2">
-            Email:
-        </td>
-    </tr>
+                <tr>
+                    <td class="s2">Phone Number:</td>
+                </tr>
+                <tr>
+                    <td class="s3"><input type="tel" required></td>
+                </tr>
 
-    <tr>
-        <td class="s3">
-            <input type="text">
-        </td>
-    </tr>
-    <tr>
-        <td class="s2">
-            Insurance Provider:
-        </td>
-    </tr>
+                <tr>
+                    <td class="s2">Email:</td>
+                </tr>
+                <tr>
+                    <td class="s3"><input type="email" required></td>
+                </tr>
 
-    <tr>
-        <td class="s3">
-            <select> 
-                <option>Select Provider</option>
-                <option>Provider 1</option>
-                <option>Provider 2</option>
-                <option>Provider 3</option>
-            </select>
+                <tr>
+                    <td class="s2">Insurance Provider:</td>
+                </tr>
+                <tr>
+                    <td class="s3">
+                        <select required>
+                            <option>Select Provider</option>
+                            <option>Provider 1</option>
+                            <option>Provider 2</option>
+                            <option>Provider 3</option>
+                        </select>
+                    </td>
+                </tr>
 
-        </td>
-    </tr>
-    <tr>
-        <td class="s2">
-            Insurance policy number:
-        </td>
-    </tr>
+                <tr>
+                    <td class="s2">Insurance Policy Number:</td>
+                </tr>
+                <tr>
+                    <td class="s3"><input type="text" required></td>
+                </tr>
 
-    <tr>
-        <td class="s3">
-            <input type="text">
-        </td>
-    </tr>
-    <tr><td class = "a"><h1><center>Additional Information</center></h1></td></tr>
-    
-    <tr>
-        <td class="s2">
-            Username:
-        </td>
-    </tr>
+                <tr><td class="a"><h2>Additional Information</h2></td></tr>
 
-    <tr>
-        <td class="s3">
-            <input type="text">
-        </td>
-    </tr>
-    <tr>
-        <td class="s2">
-            Password:
-        </td>
-    </tr>
+                <tr>
+                    <td class="s2">Username:</td>
+                </tr>
+                <tr>
+                    <td class="s3"><input type="text" required></td>
+                </tr>
 
-    <tr>
-        <td class="s3">
-            <input type="text">
-        </td>
-    </tr>
-    <tr>
-        <td class="s2">
-            Confirm Password:
-        </td>
-    </tr>
+                <tr>
+                    <td class="s2">Password:</td>
+                </tr>
+                <tr>
+                    <td class="s3"><input type="password" required></td>
+                </tr>
 
-    <tr>
-        <td class="s3">
-            <input type="text">
-        </td>
-    </tr>
+                <tr>
+                    <td class="s2">Confirm Password:</td>
+                </tr>
+                <tr>
+                    <td class="s3"><input type="password" required></td>
+                </tr>
 
-
-    <tr><td>
-        <input type="Submit" class="button" value="Register"><br/><br/>
-    </td></tr>
-
-
-
-</center>
-</div>
-</table>
+                <tr>
+                    <td><input type="submit" class="button" value="Register"></td>
+                </tr>
+            </table>
+        </form>
+    </div>
 </body>
 </html>
-
