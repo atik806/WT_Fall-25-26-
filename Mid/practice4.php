2fa2f4fe15e9c64cf8ca0af5ef34b9e29d37b54f
atik806 <atikrj8@gmail.com>
2025-11-16 12:48:00 +0600

    practice 4

---
+++ Mid/practice4.php
@@ -0,0 +1,64 @@
+<!Doctype html>
+<html>
+    <head>
+        <title>
+            Daily-Flash
+        </title>
+    </head>
+    <style>
+        h1{
+            text-align: center;
+            margin-top: 50px;
+        }
+        h2{
+            text-align: center;
+            margin-top: -10px;
+        }
+        nav{
+            border: 2px solid black;
+            background-color: #f0e68cff;
+            padding: 10px;
+            color: white;   
+            text-align: center;
+            margin-top: 20px;
+            display: flex;
+            justify-content: center;
+            gap: 30px;
+
+        }
+        .s1{
+            width: 70%;
+        }
+        .h2{
+            
+            margin-bottom: -10px;
+        }
+    </style>
+    <body>
+        
+    <h1>
+        Daily-Flash
+    </h1>
+    <h2>Your Source for Instant News</h2>
+    <nav>
+        <a href="#Home">Home</a>
+        <a href="World">World</a>
+        <a href="#Lifestyle">Lifestyle</a>
+        <a href="tech">Tech</a>
+    </nav>
+    
+
+    <section class="s1">
+        <h2 class="h2">Global Summit on Climate Change Concludes</h2>
+        <p>
+            Top leaders from around the world gathered today to discuss global climate strategies. 
+               The summit focused on reducing carbon emissions and promoting sustainable energy solutions. 
+               Experts believe this meeting will significantly shape future environmental policies.
+        </p>
+        
+    </section>
+
+
+
+    </body>
+</html>
