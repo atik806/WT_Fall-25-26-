d2715651e3d02752ae5be10aa95a0a2189b27473
atik806 <atikrj8@gmail.com>
2025-12-30 19:35:32 +0600

    new update

--- Final/Main Project/Management/User/MVC/html/register.php
+++ Final/Main Project/Management/User/MVC/html/register.php
@@ -8,11 +8,13 @@
 </head>
 
 <body>
+    
 
     <div class="a">
         <h2>Create Account</h2>
 
-        <form method="POST">
+        <form method="POST" action="register.php">
+
     <label>Full Name</label>
     <input type="text" name="fullname" placeholder="Enter your name" required>
 
@@ -29,52 +31,13 @@
 </form>
 
 
+
         <div class="login">
             Already have an account? <a href="login.php">Login</a>
         </div>
     </div>
 
-<?php
-include "<db>db.php";
-$success = "";
-$error = "";
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    $fullname = trim($_POST['fullname']);
-    $phone = trim($_POST['phone']);
-    $email = trim($_POST['email']);
-    $password = trim($_POST['password']);
-
-    if(empty($fullname) || empty($phone) || empty($email) || empty($password)){
-        die("All fields are required");
-    }
-
-    if(preg_match('/[0-9]/', $fullname)){
-        die("Name must not contain numbers");
-    }
-
-    if(!preg_match('/^01[0-9]{9}$/', $phone)){
-        die("Please enter a valid phone number");
-    }
-
-    if(!preg_match("/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/", $email)){
-        die("Please enter a valid email address (e.g., example@domain.com)");
-    }
-
-    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-
-    $sql = "INSERT INTO user (fullname, phone, email, password) VALUES ('$fullname', '$phone', '$email', '$hashedPassword')";
-
-    if ($conn->query($sql)) {
-        $success = "Registration successful!";  
-        
-
-    } else {
-        $error="ERROR ". $conn->error;
-    }
-
-    $conn->close();
-}
-?>
+    
 
 
 </body>
