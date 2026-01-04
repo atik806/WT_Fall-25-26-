46665dae55d7f117c6a73c4ed074620873ddd5bf
atik806 <atikrj8@gmail.com>
2026-01-04 11:51:16 +0600

    update2

---
+++ Final/Main Project/Management/Organizer/MVC/html/createEvent.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Create Event</title>
+    <link rel="stylesheet" href="../css/createEvent.css">
+</head>
+<body>
+
+    <div class="event-form">
+        <h2>Create Event</h2>
+
+        <form>
+            <label>Event Name</label><br>
+            <input type="text" placeholder="Enter event name"><br><br>
+
+            <label>Date</label><br>
+            <input type="date"><br><br>
+
+            <label>Time</label><br>
+            <input type="time"><br><br>
+
+            <label>Location</label><br>
+            <input type="text" placeholder="Enter location"><br><br>
+
+            <label>Description</label><br>
+            <textarea placeholder="Event description"></textarea><br><br>
+
+            <button type="submit">Create Event</button>
+        </form>
+    </div>
+
+</body>
+</html>
--- Final/Main Project/Management/User/MVC/php/login.php
+++ Final/Main Project/Management/User/MVC/php/login.php
@@ -1,24 +1,31 @@
-<?php
-include "../db/db.php";
-$success = "";
-$error = "";
+    <?php
+    include "../db/db.php";
+    $success = "";
+    $error = "";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $email = trim($_POST['email']);
-    $password = trim($_POST['password']);
-    if (empty($email) || empty($password)) {
-        die("Both fields are required");
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $email = trim($_POST['email']);
+        $password = trim($_POST['password']);
+        if (empty($email) || empty($password)) {
+            die("Both fields are required");
+        }
+        $sql = "SELECT * FROM user WHERE email='$email' AND password='$password'";
+
+
+
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            $success = "Login successful!";
+            header("Location: ../html/userDashboard.php");
+            exit();
+        } else {
+            $error = "Invalid email or password.";
+        }
+        
+        $conn->close();
+        echo $success;
+        echo $error;
     }
-    $sql = "SELECT * FROM user WHERE email='$email' && password='$password'";
-    $result = $conn->query($sql);
-    if ($result->num_rows > 0) {
-        $success = "Login successful!";
-        header("Location: ../html/dashboard.php");
-        exit();
-    } else {
-        $error = "Invalid email or password.";
-    }
-    
-    $conn->close();
-}
-?>
+
+    ?>
