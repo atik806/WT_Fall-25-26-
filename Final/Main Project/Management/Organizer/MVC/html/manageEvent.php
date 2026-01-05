42498e79b1a08fdaa52ac18eb06e30c75a1e6907
atik806 <atikrj8@gmail.com>
2026-01-05 23:00:55 +0600

    update14

---
+++ Final/Main Project/Management/Organizer/MVC/html/manageEvent.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Pending Event Requests</title>
+    <link rel="stylesheet" href="../css/manageEvent.css">
+</head>
+<body>
+    <h2>Pending Event Requests</h2>
+
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Event Name</th>
+            <th>Date</th>
+            <th>Location</th>
+            <th>Description</th>
+            <th>Action</th>
+        </tr>
+
+
+    </table>
+</body>
+</html>
